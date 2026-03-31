88aeb705b628034a0623d3b3162aff56ec34c4c7
Supachok Julaupay <supachok.jul@ku.th>
2026-04-01 01:36:17 +0700

    config db

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 import os
+from app import create_app
 
-# Google Cloud Run require dynamic PORT
+app = create_app()
+
 if __name__ == "__main__":
-    from app import create_app
-    app = create_app()
+    # Google Cloud Run requires dynamic PORT
     port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port, debug=True)
